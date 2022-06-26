a1941920f0043335b89fa70da1b9a006f384ddc0
shan <2780287233@qq.com>
2022-06-26 23:29:34 +0800

    feat:FaceReco界面

---
+++ src/views/FaceReco/FaceFunction/index.jsx
@@ -0,0 +1,43 @@
+import StyleFunction from './styled'
+import people from '../../../assets/images/小人.png'
+import shield from '../../../assets/images/护盾.png'
+import facce from '../../../assets/images/扫描人脸.png'
+
+const FaceFunction = () => {
+    return (
+        <StyleFunction>
+            <div className='section'>
+                <div className="sectionOne">
+                    <div className="sectionOnetitle">功能介绍</div>
+                    <div className="box" style={{ display: 'flex', justifyContent: 'space-between', }}>
+                        <div className="boxOne">
+                            <img src={people} alt="" style={{
+                                width: '45px', height: '45px', position: 'relative',top: '50px',left: '-110px'
+                            }} />
+                            <span className="content">
+                                <h2 className="contentTitle">人脸检测定位</h2>
+                                <span className="contentxt">检测图片中的人脸并标记出人脸坐标，支持同时识别多张人脸</span>
+                            </span>
+                        </div>
+                        <div className="boxOne boxTwo">
+                            <img src={shield} alt="" style={{ width: '45px', height: '45px',position: 'relative',top: '50px',left: '-110px' }} />
+                            <span className="content">
+                                <h2 className="contentTitle">图片质量控制</h2>
+                                <span className="contentxt">分析图片中的多种特征，确保图片符合质量标准</span>
+                            </span>
+                        </div>
+                        <div className="boxOne boxTwo boxThree">
+                            <img src={facce} alt="" style={{ width: '45px', height: '45px',position: 'relative',top: '50px',left: '-170px' }} />
+                            <span className="content">
+                                <h2 className="contentTitle">150关键点定位</h2>
+                                <span className="contentxt">精准定位包括双眼、口、鼻等人脸五官关键点</span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </StyleFunction>
+    )
+
+}
+export { FaceFunction }
---
+++ src/views/FaceReco/FaceFunction/styled.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+
+export default styled.section`
+    .section .sectionOne {
+        height: 200px;
+        width: 1200px;
+        /* margin: 0 auto; */
+        /* color: #101010; */
+        margin-bottom: 250px;
+        position: relative;
+        /* background-color: green; */
+    }
+
+    .sectionOne .sectionOnetitle {
+        text-align: center;
+        font-size: 28px;
+        width: 1200px;
+        height: 80px;
+        line-height: 80px;
+        margin-top: 80px;
+        /* margin-bottom: 10px; */
+    }
+
+    .sectionOne img {
+        /* padding: 10px; */
+        /* margin-left: 10px;
+     float: left; */
+        /* background-color: #409eff; */
+        position: relative;
+        top: 60px;
+        left: -150px;
+    }
+
+    .sectionOne .boxOne {
+        height: 300px;
+        width: 200px;
+        /* float: left; */
+        /* margin-left: 10px; */
+        /* background-color: #fff;  */
+    }
+
+    .boxOne .content {
+        display: inline-block;
+        text-align: left;
+        height: 100px;
+        width: 250px;
+        /* padding-left: 20px; */
+        padding-top: 10px;
+        color: #2c3e50;
+    }
+
+    .contentxt {
+        font-size: 14px;
+        width: 300px;
+        text-align: left;
+    }
+
+    .sectionOne .boxTwo {
+        height: 300px;
+        width: 200px;
+    }
+
+    .sectionOne .boxThree {
+        height: 300px;
+        width: 1000px;
+    }
+`
